2912c8f6b864903fedb937eb1dac08b1a6da8906
SinHyuk Kim <rlatls1997@gmail.com>
2020-09-25 15:04:17 +0900

    Hello 클래스 수정

--- src/git_test2/Main.java
+++ src/git_test2/Main.java
@@ -3,8 +3,8 @@ package git_test2;
 public class Main {
 
     public static void main(String[] args) {
-    	Hello h = new Hello();
-    	h.say();
+        Hello h = new Hello("hello world");
+        h.say();
     }
 
 }
