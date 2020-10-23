cd1cbd7992767c987676392e3546b2420f0e0a5b
SinHyuk Kim <rlatls1997@gmail.com>
2020-10-23 16:06:37 +0900

    두번째:김신혁

--- src/git_test2/Main.java
+++ src/git_test2/Main.java
@@ -5,6 +5,7 @@ public class Main {
     public static void main(String[] args) {
         Hello hello = new Hello("hello world");
         System.out.println(hello.say());
+      int i = 0;
     }
 
 }
