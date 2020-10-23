c9cef37208a2d713c645e68d5f51fde2426487a5
Yoon, DeukGi <isz03072@gmail.com>
2020-10-23 16:11:52 +0900

    new commit test

--- src/git_test2/Hello.java
+++ src/git_test2/Hello.java
@@ -7,14 +7,20 @@ public class Hello {
     public Hello() {
         this("안녕하세요");
     }
+	
+	
 
     public Hello(String message) {
         this.message = message;
     }
+	
+	
 
     public String say() {
         return message;
     }
+	
+	
 
 }
 
